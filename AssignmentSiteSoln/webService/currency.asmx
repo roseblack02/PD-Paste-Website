0467f3f3dad60259a54b54f51b0fba0da95eb588
rhysblack <rhysblackrb@gmail.com>
2023-11-16 09:04:31 +0000

    trying to make the web reference. Trying.... :c
    These college computers are NOT workin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ AssignmentSiteSoln/webService/currency.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="currency.asmx.cs" Class="webService.currency" %>
---
+++ AssignmentSiteSoln/webService/currency.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace webService
+{
+    /// <summary>
+    /// Summary description for currency
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class currency : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public static double convertCurrency(double price, int currency)
+        {
+            //list containing exhange rates in order of GBP, USD, EUR
+            //GBP is base currency
+            double[] exchangeRates = { 1, 1.24, 1.15 };
+
+            return price * exchangeRates[currency];
+        }
+    }
+}
